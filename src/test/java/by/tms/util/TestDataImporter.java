9f8c08f59122c7ae91e3ea4b9315f425c68d705a
”Mikhail <ryzgunsky.mihail@gmail.com>
2022-03-11 21:45:24 +0300

    modify data importer for test

--- src/test/java/by/tms/util/TestDataImporter.java
+++ src/test/java/by/tms/util/TestDataImporter.java
@@ -5,90 +5,87 @@ import by.tms.entity.Book;
 import by.tms.entity.Genre;
 import by.tms.entity.Order;
 import by.tms.entity.Section;
-import by.tms.entity.user.Account;
 import by.tms.entity.user.Admin;
 import by.tms.entity.user.Level;
 import by.tms.entity.user.Librarian;
 import by.tms.entity.user.User;
 import by.tms.entity.user.UserData;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class TestDataImporter {
 
-    private static final TestDataImporter INSTANCE = new TestDataImporter();
+    public static void importTestData(SessionFactory sessionFactory) {
+        Session session = sessionFactory.getCurrentSession();
 
-    public static TestDataImporter getInstance() {
-        return INSTANCE;
+        Section preSchoolLiteratureSection = saveSection(session, "Дошкольная литература");
+        Section spaceSection = saveSection(session, "Космос");
+        Section animalSection = saveSection(session, "Животные");
+
+        Genre detective = saveGenre(session, "Детектив");
+        Genre roman = saveGenre(session, "Роман");
+        saveGenre(session, "Автобиография");
+
+        Author akuninAuthor = saveAuthor(session, "Борис Акунин");
+        Author pelevinAuthor = saveAuthor(session, "Виктор Пелевин");
+        saveAuthor(session,"Чарльз Буковски");
+
+        Book azazelloBook = saveBook(session, "Азазело", akuninAuthor, detective,
+                5, animalSection, 1999);
+        Book turkishGambitBook = saveBook(session, "Турецкий гамбит", akuninAuthor, roman,
+                2, spaceSection, 2003);
+        Book generationP = saveBook(session, "Generation P", pelevinAuthor, roman,
+                4, preSchoolLiteratureSection, 1999);
+
+        User user = saveUser(session, "user", "pass", "Misha", "Ryzgunsky",
+                "ryzgunsky.mihail@gmail.com", "+375445787918");
+        User newUser = saveUser(session, "newUser", "pass1", "Sasha", "Egorov",
+                "ryzgunky.mihail@gmail.com","+375445717918");
+        saveAdmin(session, "admin", "pass1", "Dima", "Morozov",
+                "dima@mail.com", "+375445344587", Level.HEAD);
+        saveAdmin(session, "newAdmin", "pass", "Misha", "Basalay",
+                "dma@mail.com", "+375445345287", Level.SECONDARY);
+        saveLibrarian(session, "newLib", "pass1", "Kirill", "Anasovich",
+                "kiill@mail.ru", "+375445322487", Level.HEAD);
+        saveLibrarian(session, "lib", "pass", "Petya", "Lukoshko",
+                "kirill@mail.ru", "+375445322457", Level.SECONDARY);
+
+        saveOrder(session, user, List.of(azazelloBook, generationP), LocalDateTime.now().plusDays(10));
+        saveOrder(session, newUser, List.of(azazelloBook, generationP, turkishGambitBook), LocalDateTime.now().plusDays(1));
+        saveOrder(session, newUser, List.of(azazelloBook, turkishGambitBook), LocalDateTime.now().plusDays(2));
     }
 
-    public void importTestData(SessionFactory sessionFactory) {
-        try (Session session = sessionFactory.openSession()) {
-            Section preSchoolLiteratureSection = saveSection(session, "Дошкольная литература");
-            Section spaceSection = saveSection(session, "Космос");
-            Section animalSection = saveSection(session, "Животные");
-
-            Genre detective = saveGenre(session, "Детектив");
-            Genre roman = saveGenre(session, "Роман");
-
-            Author akuninAuthor = saveAuthor(session, "Борис Акунин");
-            Author pelevinAuthor = saveAuthor(session, "Виктор Пелевин");
-
-            Book azazelloBook = saveBook(session, "Азазело", akuninAuthor, detective,
-                    5, animalSection, 1999);
-            Book turkishGambitBook = saveBook(session, "Турецкий гамбит", akuninAuthor, roman,
-                    2, spaceSection, 2003);
-            Book generationP = saveBook(session, "Generation P", pelevinAuthor, roman,
-                    4, preSchoolLiteratureSection, 1999);
-
-            Account user = saveUser(session, "user", "pass", "Misha", "Ryzgunsky",
-                    "ryzgunsky.mihail@gmail.com", "+375445717918");
-            saveAdmin(session, "admin", "pass", "Dima", "Basalay",
-                    "dima@mail.com", "+375445345287", Level.HEAD);
-            saveLibrarian(session, "lib", "pass", "Kirill", "Lukoshko",
-                    "kirill@mail.ru", "+375445322457", Level.SECONDARY);
-            Transaction transaction = session.beginTransaction();
-            saveOrder(session, user, List.of(azazelloBook, generationP), LocalDateTime.now().plusDays(1));
-            saveOrder(session, user, List.of(azazelloBook, generationP,turkishGambitBook), LocalDateTime.now().plusDays(1));
-            transaction.commit();
-        }
-    }
-
-    private Section saveSection(Session session, String name) {
+    private static Section saveSection(Session session, String name) {
         Section section = Section.builder().name(name).build();
         session.save(section);
         return section;
     }
 
-    private Genre saveGenre(Session session, String name) {
+    private static Genre saveGenre(Session session, String name) {
         Genre genre = Genre.builder().name(name).build();
         session.save(genre);
         return genre;
     }
 
-    private Author saveAuthor(Session session, String fullName) {
+    private static Author saveAuthor(Session session, String fullName) {
         Author author = Author.builder().fullName(fullName).build();
         session.save(author);
         return author;
     }
 
-    private Book saveBook(Session session, String name,
-                          Author author, Genre genre, int quantity, Section section, int year) {
+    private static Book saveBook(Session session, String name,
+                                 Author author, Genre genre, int quantity, Section section, int year) {
         Book book = Book.builder().name(name).author(author).genre(genre).section(section)
                 .quantity(quantity).publicationYear(year).build();
         session.save(book);
         return book;
     }
 
-    private Account saveUser(Session session, String username, String password,
-                             String name, String surname, String email, String phone) {
+    private static User saveUser(Session session, String username, String password,
+                                    String name, String surname, String email, String phone) {
         User user = User.builder().username(username).password(password)
                 .userData(UserData.builder().name(name).surname(surname)
                         .emailAddress(email).phoneNumber(phone).build()).isBanned(false).build();
@@ -96,28 +93,25 @@ public final class TestDataImporter {
         return user;
     }
 
-    private Admin saveAdmin(Session session, String username, String password,
-                            String name, String surname, String email, String phone, Level level) {
+    private static void saveAdmin(Session session, String username, String password,
+                                  String name, String surname, String email, String phone, Level level) {
         Admin admin = Admin.builder().username(username).password(password)
                 .userData(UserData.builder().name(name).surname(surname)
                         .emailAddress(email).phoneNumber(phone).build()).adminLevel(level).build();
         session.save(admin);
-        return admin;
     }
 
-    private Librarian saveLibrarian(Session session, String username, String password,
-                                    String name, String surname, String email, String phone, Level level) {
+    private static void saveLibrarian(Session session, String username, String password,
+                                      String name, String surname, String email, String phone, Level level) {
         Librarian librarian = Librarian.builder().username(username).password(password)
                 .userData(UserData.builder().name(name).surname(surname)
                         .emailAddress(email).phoneNumber(phone).build()).libLevel(level).build();
         session.save(librarian);
-        return librarian;
     }
 
-    private Order saveOrder(Session session, Account account, List<Book> books, LocalDateTime period) {
+    private static void saveOrder(Session session, User account, List<Book> books, LocalDateTime period) {
         Order order = Order.builder().account(account)
                 .book(books).rentalTime(LocalDateTime.now()).rentalPeriod(period).build();
         session.save(order);
-        return order;
     }
 }
