57989d5703fbbf62c05de4316a9d11819c71cddf
”Mikhail <ryzgunsky.mihail@gmail.com>
2022-03-26 15:43:49 +0300

    add util class for controllers

---
+++ src/main/java/by/tms/web/util/PageUtil.java
@@ -0,0 +1,77 @@
+package by.tms.web.util;
+
+public final class PageUtil {
+
+    private PageUtil() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static final String USER_PREFIX = "page/user/";
+    public static final String ADMIN_PREFIX = "page/admin/";
+    public static final String LIBRARIAN_PREFIX = "page/librarian/";
+    public static final String ERROR_PAGE_PREFIX = "page/errorPage";
+    public static final String BAN_PAGE = "page/banPage";
+    public static final String START_PAGE = "/";
+    public static final String USER_ATTRIBUTE = "user";
+    public static final String ACCOUNT_ATTRIBUTE = "account";
+    public static final String SHOPPING_CART_ATTRIBUTE = "shoppingCart";
+    public static final String BOOKS_ATTRIBUTE = "books";
+    public static final String OFFSET_LIST_ATTRIBUTE = "offsetList";
+    public static final String COUNT_PAGES_ATTRIBUTE = "countPages";
+    public static final String OFFSET_PARAMETER = "offset";
+    public static final String LIMIT_PARAMETER = "limit";
+    public static final String ALL_USERS_ATTRIBUTE = "allUsers";
+    public static final String ALL_ORDERS_ATTRIBUTE = "orders";
+    public static final String DATE_TIME_NOW_ATTRIBUTE = "dateTimeNow";
+    public static final String DEBTORS_ATTRIBUTE = "debtors";
+    public static final String LIBRARIAN_PAGE_SUFFIX = "librarian";
+    public static final String USER_PAGE_SUFFIX = "user";
+    public static final String USER_PAGE = "userPage";
+    public static final String LIBRARIAN_PAGE = "librarianPage";
+    public static final String ADMIN_PAGE = "adminPage";
+    public static final String REGISTER_PAGE = "registerPage";
+    public static final String ERROR_PAGE = "errorPage";
+    public static final String INDEX_PAGE_SUFFIX = "index";
+    public static final String ADD_BOOK_PAGE = "addBook";
+    public static final String AUTHOR_PARAMETER = "authorFullName";
+    public static final String GENRE_PARAMETER = "genre";
+    public static final String SECTION_PARAMETER = "section";
+    public static final String RENTAL_PERIOD_PARAMETER = "rentalPeriod";
+    public static final String BOOK_ID_PARAMETER = "bookId";
+    public static final String REFERER_HEADER = "referer";
+    public static final String REDIRECT = "redirect:";
+    public static final String USERNAME_PARAMETER = "username";
+    public static final String BLOCK_USER_PAGE = "blockUser";
+    public static final String ADMIN_PAGE_SUFFIX = "admin";
+    public static final String LANGUAGE_ATTRIBUTE = "language";
+    public static final String LANGUAGE_PARAMETER = "lang";
+    public static final String SHOPPING_CART_PAGE = "shoppingCart";
+    public static final String EDIT_BOOK_PAGE = "editBook";
+    public static final String SHOW_ALL_BOOKS_COMMAND = "showAllBooks";
+    public static final String FIND_USER_ORDER_PAGE = "findUserOrder";
+    public static final String USER_ORDER_PAGE = "userOrder";
+    public static final String USERS_ORDER_ATTRIBUTE = "usersOrder";
+    public static final String ORDER_ID_PARAMETER = "orderId";
+    public static final String AMPERSAND = "&";
+    public static final String EQUAL_SIGH = "=";
+    public static final String ALL_ORDERS_PAGE = "allOrders";
+    public static final String ALL_USERS_PAGE = "allUsers";
+    public static final String USER_INFO_PAGE = "userInfo";
+    public static final String AUTHORS_ATTRIBUTE = "authors";
+    public static final String GENRES_ATTRIBUTE = "genres";
+    public static final String SECTIONS_ATTRIBUTE = "sections";
+    public static final String COMMAND_PARAMETER = "command";
+    public static final String PASSWORD_PARAMETER = "password";
+    public static final String ROLE_PARAMETER = "role";
+    public static final String NAME_PARAMETER = "name";
+    public static final String SURNAME_PARAMETER = "surname";
+    public static final String PHONE_PARAMETER = "phone";
+    public static final String EMAIL_PARAMETER = "email";
+    public static final String ALL_BOOKS_PAGE = "allBooks";
+    public static final String BOOK_NAME_PARAMETER = "bookName";
+    public static final String QUANTITY_PARAMETER = "quantity";
+    public static final String YEAR_PARAMETER = "year";
+    public static final int OFFSET_ZERO = 0;
+    public static final int ONE_BOOK = 1;
+    public static final int LIMIT_TEN = 10;
+}
