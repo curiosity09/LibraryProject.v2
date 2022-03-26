fc283267362a0ac976046e69ea9055c6064a994e
”Mikhail <ryzgunsky.mihail@gmail.com>
2022-03-26 15:43:29 +0300

    add util class for services

---
+++ src/main/java/by/tms/model/util/ServiceUtil.java
@@ -0,0 +1,28 @@
+package by.tms.model.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class ServiceUtil {
+
+    private static final long LIMIT_TEN = 10;
+
+    private ServiceUtil() {
+    }
+
+    public static List<Long> collectPages(Long countRow) {
+        long countPage;
+        if (countRow % LIMIT_TEN != 0) {
+            countPage = countRow / LIMIT_TEN + 1;
+        } else {
+            countPage = countRow / LIMIT_TEN;
+        }
+        List<Long> offsetList = new ArrayList<>();
+        long num = 0;
+        for (int i = 0; i < countPage; i++) {
+            offsetList.add(num);
+            num += LIMIT_TEN;
+        }
+        return offsetList;
+    }
+}
