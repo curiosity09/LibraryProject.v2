4302887a1c411b70984562dff0c8aab78b93bfad
”Mikhail <ryzgunsky.mihail@gmail.com>
2022-03-29 16:35:37 +0300

    add log in controller advice

--- src/main/java/by/tms/web/controller/RuntimeExceptionControllerAdvice.java
+++ src/main/java/by/tms/web/controller/RuntimeExceptionControllerAdvice.java
@@ -1,15 +1,17 @@
 package by.tms.web.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class RuntimeExceptionControllerAdvice {
 
     @ExceptionHandler(RuntimeException.class)
     public String handleError(Model model, RuntimeException exception){
-        //Logger.error("text here", exception);
+        log.debug(exception.getMessage(), exception);
         model.addAttribute("exceptionMessage", exception.getMessage());
         return "page/errorPage";
     }
