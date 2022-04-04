e0d2fa54503dd3586df85ad973973078d372786a
”Mikhail <ryzgunsky.mihail@gmail.com>
2022-04-04 23:09:02 +0300

    delete useless elements in pages

--- src/main/java/by/tms/web/controller/handler/RuntimeExceptionControllerAdvice.java
+++ src/main/java/by/tms/web/controller/handler/RuntimeExceptionControllerAdvice.java
@@ -1,5 +1,6 @@
 package by.tms.web.controller;
 
+import by.tms.web.util.PageUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,9 +11,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class RuntimeExceptionControllerAdvice {
 
     @ExceptionHandler(RuntimeException.class)
-    public String handleError(Model model, RuntimeException exception){
+    public String handleError(Model model, RuntimeException exception) {
         log.debug(exception.getMessage(), exception);
-        model.addAttribute("exceptionMessage", exception.getMessage());
-        return "page/errorPage";
+        model.addAttribute(PageUtil.EXCEPTION_MESSAGE, exception.getMessage());
+        return PageUtil.ERROR_PAGE_PREFIX;
     }
 }
