210798aa361335c2993b066b3d6e08a87907b19c
”Mikhail <ryzgunsky.mihail@gmail.com>
2022-04-08 14:28:46 +0300

    add @ComponentScan annotation in database test config

--- src/test/java/by/tms/model/config/DatabaseConfigTest.java
+++ src/test/java/by/tms/model/config/DatabaseConfigTest.java
@@ -1,9 +1,11 @@
 package by.tms.model.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import(DatabaseConfig.class)
+@ComponentScan("by.tms.model.util")
 public class DatabaseConfigTest {
 }
