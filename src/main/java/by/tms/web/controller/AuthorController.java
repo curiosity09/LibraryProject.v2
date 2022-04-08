a25a7509a4cce9f3db4346490461de6741217a0f
”Mikhail <ryzgunsky.mihail@gmail.com>
2022-04-08 14:25:22 +0300

    add validation in controllers

--- src/main/java/by/tms/web/controller/AuthorController.java
+++ src/main/java/by/tms/web/controller/AuthorController.java
@@ -8,9 +8,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 import static by.tms.web.util.PageUtil.*;
 
@@ -23,15 +28,22 @@ public class AuthorController {
 
     @GetMapping("/librarian/addAuthorPage")
     public String addAuthorPage(Model model) {
-        model.addAttribute(AUTHOR_ATTRIBUTE, AuthorDto.builder().build());
+        model.addAttribute(AUTHOR_ATTRIBUTE, new AuthorDto());
         return LIBRARIAN_PREFIX + ADD_AUTHOR_SUFFIX;
     }
 
-    @PostMapping("/librarian/addAuthor")
-    public String addAuthor(AuthorDto author) {
-        authorService.save(author);
-        log.debug(LoggerUtil.ENTITY_WAS_SAVED_IN_CONTROLLER, author);
-        return REDIRECT + LIBRARIAN_PAGE;
+    @PostMapping("/addAuthor")
+    public String addAuthor(@ModelAttribute(AUTHOR_ATTRIBUTE) @Valid AuthorDto author, BindingResult result) {
+        if (result.hasErrors()) {
+            return LIBRARIAN_PREFIX + ADD_AUTHOR_SUFFIX;
+        }
+        Optional<AuthorDto> authorByFullName = authorService.findAuthorByFullName(author.getFullName());
+        if (authorByFullName.isEmpty()) {
+            authorService.save(author);
+            log.debug(LoggerUtil.ENTITY_WAS_SAVED_IN_CONTROLLER, author);
+            return REDIRECT + LIBRARIAN_PAGE;
+        }
+        return REDIRECT + ERROR_PAGE;
     }
 
     @GetMapping("/user/allAuthorPage")
--- src/main/java/by/tms/web/controller/GenreController.java
+++ src/main/java/by/tms/web/controller/GenreController.java
@@ -8,8 +8,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+import java.util.Optional;
 
 import static by.tms.web.util.PageUtil.*;
 
@@ -27,9 +33,16 @@ public class GenreController {
     }
 
     @PostMapping("/addGenre")
-    public String addGenre(GenreDto genre) {
-        genreService.save(genre);
-        log.debug(LoggerUtil.ENTITY_WAS_SAVED_IN_CONTROLLER, genre);
-        return REDIRECT + LIBRARIAN_PAGE;
+    public String addGenre(@ModelAttribute(GENRE_ATTRIBUTE) @Valid GenreDto genre, Errors errors) {
+        if (errors.hasErrors()) {
+            return LIBRARIAN_PREFIX + ADD_GENRE_SUFFIX;
+        }
+        Optional<GenreDto> genreByName = genreService.findGenreByName(genre.getName());
+        if (genreByName.isEmpty()) {
+            genreService.save(genre);
+            log.debug(LoggerUtil.ENTITY_WAS_SAVED_IN_CONTROLLER, genre);
+            return REDIRECT + LIBRARIAN_PAGE;
+        }
+        return REDIRECT + ERROR_PAGE;
     }
 }
